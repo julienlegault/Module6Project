04720a2fd087cf50a2c22bf2eee7245a2ede25f8
julienlegault <julien.p.legault@gmail.com>
2021-07-14 23:15:13 -0400

    Can now import leagues and add leagues

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-import LeagueManagerLeagueEditUI
-import LeagueManagerTeamEditUI
-from LeagueManagerUI import Ui_LeagueManager
-from commonUI.commonUI import commonUI
+import sys
 
-UI = commonUI()
-UI.setupUI()
+from PyQt5 import QtWidgets
+
+from commonUI.LeagueManagerUI import Ui_LeagueManager
+
+app = QtWidgets.QApplication(sys.argv)
+LeagueManager = QtWidgets.QMainWindow()
+ui = Ui_LeagueManager()
+ui.setupUi(LeagueManager)
+ui.LoadButton.clicked.connect(lambda: ui.loadTeams())
+ui.AddButton.clicked.connect(lambda: ui.Add_League_Modal())
+LeagueManager.show()
+sys.exit(app.exec_())
